8e3e25017a25b350d1ae64537d23bf04d06b70e9
Oumar3 <oumaralitori1@gmail.com>
2025-10-29 10:46:44 +0100

    refact: update .env added new uri for mongodb

--- backend/src/config/db.conf.js
+++ backend/src/config/db.conf.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI)
+    await mongoose.connect(process.env.MongoDB_URI_PROD)
       .then((res) => {
         console.log('MongoDB connected');
       })
