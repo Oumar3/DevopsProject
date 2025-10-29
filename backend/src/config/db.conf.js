ccd6883f06e2f2dce01a823fa66eb65312d17255
Oumar3 <oumaralitori1@gmail.com>
2025-10-29 10:51:30 +0100

    fix: update MongoDB connection URI to use MONGODB_URI environment variable

--- backend/src/config/db.conf.js
+++ backend/src/config/db.conf.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MongoDB_URI_PROD)
+    await mongoose.connect(process.env.MONGODB_URI)
       .then((res) => {
         console.log('MongoDB connected');
       })
